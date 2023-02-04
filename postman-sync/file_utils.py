a136277d0b7a2a6fb65502859f1d3f63ce17efa7
Noah <burghardtnoah@gmail.com>
2023-02-03 17:54:34 -0700

    removed python component

--- postman-sync/file_utils.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def load_json_from_file(fname):
-    with open(fname) as f:
-        return json.loads(f.read())
-
-def save_json_to_file(fname, collection):
-    with open(fname, 'w') as f:
-        f.write(json.dumps(collection, indent=4))
